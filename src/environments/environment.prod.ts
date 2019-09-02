be228e00bdd9fb31c1877c69a36729a4fdb3179d
Julio Pérez <soyJulioPerez@outlook.com>
2019-09-02 09:53:25 -0300

    setup: instalar firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBKM08A9WHEhTLXbY7CxF49gN5fl-hk8f8",
+    authDomain: "febocak-02.firebaseapp.com",
+    databaseURL: "https://febocak-02.firebaseio.com",
+    projectId: "febocak-02",
+    storageBucket: "",
+    messagingSenderId: "850669499931",
+    appId: "1:850669499931:web:53e8e8e339850d33"
+    }
 };
