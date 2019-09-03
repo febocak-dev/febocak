6c7728df917b3aabea6e7ace577b041a2e6f362d
Julio Pérez <soyJulioPerez@outlook.com>
2019-09-03 09:23:28 -0300

    deploy: deploy a github-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
+  /* firebaseConfig: {
     apiKey: "AIzaSyBKM08A9WHEhTLXbY7CxF49gN5fl-hk8f8",
     authDomain: "febocak-02.firebaseapp.com",
     databaseURL: "https://febocak-02.firebaseio.com",
@@ -8,5 +8,14 @@ export const environment = {
     storageBucket: "",
     messagingSenderId: "850669499931",
     appId: "1:850669499931:web:53e8e8e339850d33"
-    }
+    } */
+    firebaseConfig: {
+      apiKey: "AIzaSyDbLNWzHhl1dUWFohXzkv1U7ymzgAZgy68",
+      authDomain: "fobokac-01.firebaseapp.com",
+      databaseURL: "https://fobokac-01.firebaseio.com",
+      projectId: "fobokac-01",
+      storageBucket: "fobokac-01.appspot.com",
+      messagingSenderId: "1019734820364"
+    },
+    modulo: 'Inscripciones'
 };
