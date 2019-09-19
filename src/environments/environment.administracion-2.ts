5b9bfcff8ef25cf65996348a5199ccebb6f93f21
Julio Pérez <soyJulioPerez@outlook.com>
2019-09-19 14:54:03 -0300

    deploy: environments files
    - cree administracion-1 para ejecutar módulo de administración con
    la base de datos de prueba y administracion-2 para ejecutarla con
    la base de datos de produccion
    - cree inscripcion-1 e inscripciion-2 para ejecutar el módulo de
    inscripciones
    - hice los cambios necesarios en angular.json para esas configuraciones
    - cree comandos en package.json para ejecutar el módulo deseado más
    facilmente

--- src/environments/environment.administracion-2.ts
+++ src/environments/environment.administracion-2.ts
@@ -8,14 +8,6 @@ export const environment = {
     storageBucket: "",
     messagingSenderId: "850669499931",
     appId: "1:850669499931:web:53e8e8e339850d33"
-    },
-    /* firebaseConfig: {
-      apiKey: "AIzaSyDbLNWzHhl1dUWFohXzkv1U7ymzgAZgy68",
-      authDomain: "fobokac-01.firebaseapp.com",
-      databaseURL: "https://fobokac-01.firebaseio.com",
-      projectId: "fobokac-01",
-      storageBucket: "fobokac-01.appspot.com",
-      messagingSenderId: "1019734820364"
-    }, */
-    modulo: 'Administración'
+  },
+  modulo: "Administración"
 };
