d0f6254791f741fa9f2411555f1093b866d484c7
Julio Pérez <soyJulioPerez@outlook.com>
2019-11-29 07:32:12 -0300

    change: al seleccionar competencia actual copiar distancias
    - antes las distancias estaban dentro de las competencias
    - ahora las distancias están dentro del tipo de competencia
    - Por lo tanto al seleccionar la competencia actual hay que
      copiar las distancias correspondientes a su tipo

--- src/app/procedures/consola/components/modal-competencia.component.ts
+++ src/app/procedures/consola/components/modal-competencia.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { CompetenciaI } from 'src/app/models/competencia';
+import { CrudService } from '@services/crud.service';
+import { TipoDeCompetenciaI } from '@models/tipo-de-competencia';
 
 @Component({
   selector: 'app-modal-competencia',
@@ -11,12 +13,21 @@ export class ModalCompetenciaComponent implements OnInit {
   @Input() tabla: CompetenciaI[];
   @Output() emitSeleccionar = new EventEmitter<CompetenciaI>();
 
-  constructor() { }
+  constructor(private crudService: CrudService) { }
 
   ngOnInit() {
   }
 
-  seleccionar(registro: CompetenciaI) {
+  async seleccionar(registro: CompetenciaI) {
+    console.log('TCL: ModalCompetenciaComponent -> seleccionar -> registro antes', registro)
+
+    const tipoDeCompetencia = 
+      await this.crudService.getRecordByField$<TipoDeCompetenciaI>('tipos-de-competencias', 'tipo', registro.tipoDeCompetencia).toPromise()
+    registro = {...registro, distancia: tipoDeCompetencia.distancia} 
+    
+    console.log('TCL: ModalCompetenciaComponent -> seleccionar -> registro despues', registro)
+      await this.crudService.updateRecord$('competencias', registro.id, registro).toPromise()
+
     this.emitSeleccionar.emit(registro);
     this.btClose.nativeElement.click();
   }
